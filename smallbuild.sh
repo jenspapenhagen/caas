a7342e11e6f533c869542407f5ee7e9b6eb6b2c3
jenspapenhagen <jens.papenhagen@web.de>
2018-10-27 12:03:44 +0200

    staring with a docker compose and not using a bash shell ;)

--- smallbuild.sh
+++ smallbuild.sh
@@ -1,9 +1,20 @@
+cd docker/prometheus
+docker build -t base/prometheus .
+docker remove localhost:5000/base/prometheus
+docker push localhost:5000/base/prometheus
+docker kill base/prometheus
+docker rm base/prometheus
+
+cd ..
+cd ..
 mvn clean install
 docker build -t base/app .
 docker remove localhost:5000/base/app
 docker push localhost:5000/base/app
 docker kill base/app
 docker rm base/app
+
+docker-compose up -d
 
 #need to get finale line soon
 #docker run -d -p 8082:8080 -p 9992:9990 -u serveradmin:serveradmin --name caas -v /opt/wildfly/standalone/log:/opt/wildfly-14.0.1.Final/standalone/log -v /opt/wildfly/standalone/deployments:/opt/wildfly-14.0.1.Final/standalone/deployments -v /opt/wildfly/standalone/configuration:/opt/wildfly-14.0.1.Final/standalone/configuration localhost:5000/base/app /opt/jboss/wildfly/bin/standalone.sh -c standalone-full.xml -b 0.0.0.0 -bmanagement 0.0.0.0
